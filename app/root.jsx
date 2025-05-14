eee1261c124b737706affdb4c1c20f43caeabd35
abhaysam2888 <abhaysam2888@gmail.com>
2025-05-14 10:11:58 +0530

    google adsense added

--- app/root.jsx
+++ app/root.jsx
@@ -31,9 +31,10 @@ export const links = () => [
   },
   {
     rel: 'icon',
-    type: "image/x-icon",
+    type: 'image/x-icon',
     href: './favicon.ico',
   },
+  { name: 'google-adsense-account', content: 'ca-pub-7469479382238786' }, // 👈 Google AdSense meta
 ]
 
 // Layout component for consistent structure
@@ -45,14 +46,17 @@ export function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <DynamicLinks /> 
+        <DynamicLinks />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-7LPX1RTT5T"
         ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/components/prism-javascript.min.js"></script>
-        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <script
+          async
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
